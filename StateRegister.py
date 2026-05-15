239a7a23751b694bd92d06a8c957962525308c69
Jahongir Mahammadiyev <91736357+Jahongir0124@users.noreply.github.com>
2026-05-15 09:11:39 +0500

    Update StateRegister.py

--- StateRegister.py
+++ StateRegister.py
@@ -20,18 +20,7 @@ class RegisterState(StatesGroup):
 
 
 state_titles = {
-    "full_name": "To'liq ismingizni kiriting:",
-    "phone_number": "Telefon raqamingizni kiriting:",
-    "about_project": "Loyihangiz haqida ma'lumot bering:",
-    "active_time": "Qancha vaqtda beri faoliyat yuritasiz?",
-    "income": "Daromadingiz oylik yoki yillik?",
-    "partner": "Sherikchilikda ishlab kop'rganmisiz?",
-    "need_invest": "Nima sababdan investitsiyaga muhtojsiz?",
-    "spend": "Investitsiya olgan mablagini qayerga sarflamoqchisiz?",
-    "return_time": "Qancha vaqtda bu mablag'ni qaytara olasiz?",
-    "multiply_income": "Investitsiya olgandan keyn daromadi nechi barobarga kopayadi?",
-    "privilege_time": "Investor sizga qancha vaqt imtiyozli davr qilib berishini xoxlaysiz?",
-    "can_suggest": "Investorga qanaqa qulay takliflar bera oladi?",
+   
     
 }
 
